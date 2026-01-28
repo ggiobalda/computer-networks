2d85b3431a36100cbb51ad6e69767be595fa66ae
ggiobalda <giorgio.baldacci04@gmail.com>
2026-01-29 00:58:55 +0100

    Fixed board print, added gitignore

--- lavagna.c
+++ lavagna.c
@@ -5,9 +5,12 @@ int main() {
 	Board* kanban = create_board(BOARD_PORT);
 
 	// creazione di qualche card iniziale
-	for (int i = 0; i < 5; i++)
-		add_card(kanban, TODO, "Card" + i);
-		
+	for (int i = 0; i < 5; i++) {
+		char desc[MAX_CHARS];
+		sprintf(desc, "Card%d", i);
+		add_card(kanban, TODO, desc);
+	}
+
 	print_board(kanban);
 
 	return 0;
